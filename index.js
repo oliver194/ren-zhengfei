3a902ea05cdd17f63d038a4f4b8b8db7beb64a34
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-07-15 18:14:00 +0300

    fix unknown command error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = import("node-fetch");
 
 async function HttpRequest(method, url) {
   return await (await fetch).default(url, { method: method });
-};
+}
 
 var package_config = require("./package.json");
 
@@ -30,29 +30,23 @@ client.on("messageCreate", (msg) => {
   if (msg.content.indexOf(config.prefix) !== 0) return;
   const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  try {
-    const fs = require("fs");
-    fs.readdir("./commands", (err, files) => {
-      if (err) console.error(err);
-      else {
-        files.forEach((file) => {
-          if (file.endsWith(".js") && file.split(".")[0] === command) {
-            const commandFile = require(`./commands/${file}`);
-            commandFile.execute(client, msg, args, config);
-          }
-        });
-      }
-    });
-  } catch (err) {
-    if (err.code === "MODULE_NOT_FOUND") {
-      msg.reply("Invalid command!").then((message) => {
-        setTimeout(() => {
-          msg.delete();
-          message.delete();
-        }, 5000);
-      });
+  const fs = require("fs");
+  var err, files = fs.readdirSync("./commands");
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    if (file.endsWith(".js") && file.split(".")[0] === command) {
+      var commandFile = require(`./commands/${file}`);
+      commandFile.execute(client, msg, args, config);
+      return;
     }
   }
+
+  msg.reply(`Invalid command!`).then((message) => {
+    setTimeout(() => {
+      msg.delete();
+      message.delete();
+    }, 5000);
+  });
 });
 
 client.login(process.env.DISCORD_TOKEN);
