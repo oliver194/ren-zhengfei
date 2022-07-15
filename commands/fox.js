75911488b1b877825cabb61b68986378b6ad8c9a
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-15 13:05:15 +0100

    Create fox.js

---
+++ commands/fox.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "cat",
+  description: "Command providing a random cat image",
+  execute(message, args, config) {
+    if (message.channel.id !== config.animal_images_channel) {
+      message
+        .reply("This command can only be used in the animal images channel!")
+        .then((msg) => {
+          setTimeout(() => {
+            msg.delete();
+            message.delete();
+          }, 5000);
+        });
+      return;
+    }
+    config.request("GET", "https://randomfox.ca/floof").then((response) => {
+      response.json().then((data) => {
+        const foxEmbed = new MessageEmbed()
+          .setColor(config.color)
+          .setTitle("Here's a fox!")
+          .setImage(data.image)
+          .setTimestamp()
+          .setFooter({ text: "Powered by randomfox.ca" });
+        message.reply({ embeds: [foxEmbed] });
+      });
+    });
+  },
+};
