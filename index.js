8ffedc12074fa67ceef2e8a81d65ac0d7b5364e9
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-15 20:46:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var package_config = require("./package.json");
 
 var config = {
   version: package_config.version,
+  discordjs: package_config.discord.js,
   color: "584dff",
   prefix: "!",
   animal_images_channel: process.env.ANIMAL_CHANNEL_ID,
