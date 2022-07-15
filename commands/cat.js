57d6c0fb42fade78b9320215e3bffef3ad1813a2
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-15 13:02:04 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -4,6 +4,17 @@ module.exports = {
   name: "cat",
   description: "Command providing a random cat image",
   execute(message, args, config) {
+    if (message.channel.id !== config.animal_images_channel) {
+      message
+        .reply("This command can only be used in the animal images channel!")
+        .then((msg) => {
+          setTimeout(() => {
+            msg.delete();
+            message.delete();
+          }, 5000);
+        });
+      return;
+    }
     config.request("GET", "https://aws.random.cat/meow").then((response) => {
       response.json().then((data) => {
         const catEmbed = new MessageEmbed()
