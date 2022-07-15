27e9382a2568d0b4544ba933e575ae0c8f7ddca2
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-15 12:56:27 +0100

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "dog",
+  description: "Command providing a random dog image",
+  execute(message, args, config) {
+    config.request("GET", "https://dog.ceo/api/breeds/image/random").then((response) => {
+      response.json().then((data) => {
+        const catEmbed = new MessageEmbed()
+          .setColor(config.color)
+          .setTitle("Here's a dog!")
+          .setImage(data.message)
+          .setTimestamp()
+          .setFooter({ text: "Powered by dog.ceo" });
+        message.reply({ embeds: [catEmbed] });
+      });
+    });
+  },
+};
