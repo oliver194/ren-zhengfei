9bf3ec1db8d5c52e55bbd5a0a1f00bf35ded0de6
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-15 12:58:06 +0100

    Fix embed names

--- commands/dog.js
+++ commands/dog.js
@@ -6,13 +6,13 @@ module.exports = {
   execute(message, args, config) {
     config.request("GET", "https://dog.ceo/api/breeds/image/random").then((response) => {
       response.json().then((data) => {
-        const catEmbed = new MessageEmbed()
+        const dogEmbed = new MessageEmbed()
           .setColor(config.color)
           .setTitle("Here's a dog!")
           .setImage(data.message)
           .setTimestamp()
           .setFooter({ text: "Powered by dog.ceo" });
-        message.reply({ embeds: [catEmbed] });
+        message.reply({ embeds: [dogEmbed] });
       });
     });
   },
