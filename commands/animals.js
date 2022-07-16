2648f0e6e1d94e81adacd784b2c23cbf2c2c17be
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 16:52:54 +0100

    Update animals.js

--- commands/animals.js
+++ commands/animals.js
@@ -28,7 +28,7 @@ async function execute(client, interaction, subinteraction, config) {
     .setDescription(`See the list below for available commands.\n**THE COMMANDS LISTED HERE ARE ONLY USABLE IN <#${config.animal_images_channel}>**`)
       .addFields(
           { name: "Image commands:", value: "```/cat - sends a cat image\n/fox - sends a fox image\n/dog - sends a dog image\n/lizard - sends a lizard image\n/duck - sends a duck image.\n/rpanda - sends a red panda image\n/kangaroo - sends a kangaroo image\n/koala - sends a koala image\n/raccoon - sends a raccoon image```", inline: true },
-          { name: "Fact commands:", value: "```/catfact - sends a cat fact\n/dogfact - sends a dog fact\n/birdfact - sends a bird fact\n/pandafact - sends a panda fact\n!/fact - sends a koala fact\n/raccoonfact - sends a raccoon fact```", inline: true }
+          { name: "Fact commands:", value: "```/catfact - sends a cat fact\n/dogfact - sends a dog fact\n/birdfact - sends a bird fact\n/pandafact - sends a panda fact\n/koalafact - sends a koala fact\n/raccoonfact - sends a raccoon fact```", inline: true }
       )
     .setTimestamp()
     .setFooter({ text: `Version: ${config.version}` });
