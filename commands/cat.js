8392da53545c541a1beccb18ba13edd18ae42e62
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 15:06:54 +0100

    Version 2.0.0-b1-16.07 rewrite

--- commands/cat.js
+++ commands/cat.js
@@ -1,30 +1,59 @@
-const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "cat",
-  description: "Command providing a random cat image",
-  execute(message, args, config) {
-    if (message.channel.id !== config.animal_images_channel) {
-      message
-        .reply(`This command can only be used in <#${config.animal_images_channel}>`)
-        .then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            message.delete();
-          }, 5000);
-        });
+const data = new SlashCommandBuilder()
+  .setName("cat")
+  .setDescription("Sends a random cat picture!");
+
+async function execute(client, interaction, subinteraction, config) {
+  async function get_cat_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newcat")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        .setEmoji("😻")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://aws.random.cat/meow");
+    var response = await response.json();
+    var cat_url = response.file;
+    var embed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a cat!")
+      .setImage(cat_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by aws.random.cat" });
+    return { embeds: [embed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
       return;
     }
-    config.request("GET", "https://aws.random.cat/meow").then((response) => {
-      response.json().then((data) => {
-        const catEmbed = new MessageEmbed()
-          .setColor(config.color)
-          .setTitle("Here's a cat!")
-          .setImage(data.file)
-          .setTimestamp()
-          .setFooter({ text: "Powered by aws.random.cat" });
-        message.reply({ embeds: [catEmbed] });
-      });
-    });
-  },
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newcat") {
+      await interaction.message.edit(await get_cat_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== config.animal_images_channel) {
+        
+        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_cat_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
 };
