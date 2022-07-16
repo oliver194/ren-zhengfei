c29b00825f64c2f7785aa76edf7347eef950abf4
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 16:04:21 +0100

    Version 2.0.0-b1-16.07 rewrite

--- commands/neko.js
+++ commands/neko.js
@@ -1,30 +1,59 @@
-const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "neko",
-  description: "Command providing a random neko image",
-  execute(message, args, config) {
-    if (message.channel.id !== '996019749911789618') {
-      message
-        .reply("This command can only be used in <#996019749911789618>")
-        .then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            message.delete();
-          }, 5000);
-        });
+const data = new SlashCommandBuilder()
+  .setName("neko")
+  .setDescription("Sends a random neko picture!");
+
+async function execute(client, interaction, subinteraction, config) {
+  async function get_neko_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newneko")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        //.setEmoji("")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://nekos.life/api/v2/img/neko");
+    var response = await response.json();
+    var neko_url = response.url;
+    var nekoEmbed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a neko!")
+      .setImage(neko_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by nekos.life" });
+    return { embeds: [nekoEmbed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
       return;
     }
-    config.request("GET", "https://nekos.life/api/v2/img/neko").then((response) => {
-      response.json().then((data) => {
-        const nekoEmbed = new MessageEmbed()
-          .setColor(config.color)
-          .setTitle("Here's a neko!")
-          .setImage(data.url)
-          .setTimestamp()
-          .setFooter({ text: "Powered by nekos.life" });
-        message.reply({ embeds: [nekoEmbed] });
-      });
-    });
-  },
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newneko") {
+      await interaction.message.edit(await get_neko_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== '996019749911789618') {
+        
+        await interaction.reply({ content: `This command can only be used in <#996019749911789618>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_neko_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
 };
