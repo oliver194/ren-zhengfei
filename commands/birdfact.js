fed9d26ca37eb96bb955e23b899eed04af5a7237
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 15:14:49 +0100

    Version 2.0.0-b1-16.07 rewrite

--- commands/birdfact.js
+++ commands/birdfact.js
@@ -1,30 +1,59 @@
-const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "birdfact",
-  description: "Command providing a random bird fact",
-  execute(message, args, config) {
-    if (message.channel.id !== config.animal_images_channel) {
-      message
-        .reply(`This command can only be used in <#${config.animal_images_channel}>`)
-        .then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            message.delete();
-          }, 5000);
-        });
+const data = new SlashCommandBuilder()
+  .setName("birdfact")
+  .setDescription("Command providing a random bird fact");
+
+async function execute(client, interaction, subinteraction, config) {
+  async function get_bfact_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newbfact")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        .setEmoji("🐦")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://some-random-api.ml/facts/bird");
+    var response = await response.json();
+    var bfact_url = response.fact;
+    var embed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a random bird fact!")
+      .setDescription(bfact_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by some-random-api.ml" });
+    return { embeds: [embed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
       return;
     }
-    config.request("GET", "https://some-random-api.ml/facts/bird").then((response) => {
-      response.json().then((data) => {
-        const birdfactEmbed = new MessageEmbed()
-          .setColor(config.color)
-          .setTitle("Here's a random bird fact!")
-          .setDescription(data.fact)
-          .setTimestamp()
-          .setFooter({ text: "Powered by some-random-api.ml" });
-        message.reply({ embeds: [birdfactEmbed] });
-      });
-    });
-  },
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newbfact") {
+      await interaction.message.edit(await get_bfact_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== config.animal_images_channel) {
+        
+        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_bfact_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
 };
