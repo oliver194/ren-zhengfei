e7f0fc7d27bd745bef4ba761f36568dcb8b64c18
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 15:59:37 +0100

    Version 2.0.0-b1-16.07 rewrite

--- commands/lizard.js
+++ commands/lizard.js
@@ -1,30 +1,59 @@
-const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "lizard",
-  description: "Command providing a random lizard image",
-  execute(message, args, config) {
-    if (message.channel.id !== config.animal_images_channel) {
-      message
-        .reply(`This command can only be used in <#${config.animal_images_channel}>`)
-        .then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            message.delete();
-          }, 5000);
-        });
+const data = new SlashCommandBuilder()
+  .setName("lizard")
+  .setDescription("Sends a random lizard picture!");
+
+async function execute(client, interaction, subinteraction, config) {
+  async function get_lizard_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newlizard")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        .setEmoji("🐨")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://nekos.life/api/v2/img/lizard");
+    var response = await response.json();
+    var lizard_url = response.url;
+    var lizardEmbed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a lizard!")
+      .setImage(lizard_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by nekos.life" });
+    return { embeds: [lizardEmbed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
       return;
     }
-    config.request("GET", "https://nekos.life/api/v2/img/lizard").then((response) => {
-      response.json().then((data) => {
-        const lizardEmbed = new MessageEmbed()
-          .setColor(config.color)
-          .setTitle("Here's a lizard!")
-          .setImage(data.url)
-          .setTimestamp()
-          .setFooter({ text: "Powered by nekos.life" });
-        message.reply({ embeds: [lizardEmbed] });
-      });
-    });
-  },
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newlizard") {
+      await interaction.message.edit(await get_lizard_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== config.animal_images_channel) {
+        
+        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_lizard_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
 };
