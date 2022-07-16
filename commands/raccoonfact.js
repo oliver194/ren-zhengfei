bb046e2ce4364f4354bbc6888bbef3ca15dcee6b
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 16:17:46 +0100

    Version 2.0.0-b1-16.07 rewrite

---
+++ commands/raccoonfact.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+
+const data = new SlashCommandBuilder()
+  .setName("raccoonfact")
+  .setDescription("Sends a random raccoon fact!");
+
+async function execute(client, interaction, subinteraction, config) {
+  async function get_rfact_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newrfact")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        .setEmoji("🦝")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://some-random-api.ml/facts/raccoon");
+    var response = await response.json();
+    var rfact_url = response.fact;
+    var rfactEmbed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a random raccoon fact!")
+      .setDescription(rfact_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by some-random-api.ml" });
+    return { embeds: [rfactEmbed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
+      return;
+    }
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newrfact") {
+      await interaction.message.edit(await get_rfact_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== config.animal_images_channel) {
+        
+        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_rfact_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
+};
