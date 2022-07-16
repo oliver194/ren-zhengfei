a94ab78a920d2e40ecb0ec8ed8348f99d5d50d83
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 09:37:34 +0100

    Update animals.js

--- commands/animals.js
+++ commands/animals.js
@@ -10,8 +10,8 @@ module.exports = {
       .setTitle("Animal commands")
       .setDescription(`See the list below for available commands.\n**THE COMMANDS LISTED HERE ARE ONLY USABLE IN <#${config.animal_images_channel}>**`)
       .addFields(
-          { name: "Image commands:", value: "```!cat - sends a cat image\n!fox - sends a fox image\n!dog - sends a dog image\n!lizard - sends a lizard image\n!duck - sends a duck image.\n!rpanda - sends a red panda image\n!kangaroo - sends a kangaroo image\n!koala - sends a koala image```", inline: true },
-          { name: "Fact commands:", value: "```!catfact - sends a cat fact\n!dogfact - sends a dog fact\n!birdfact - sends a bird fact\n!pfact - sends a panda fact\n!kfact - sends a koala fact```", inline: true }
+          { name: "Image commands:", value: "```!cat - sends a cat image\n!fox - sends a fox image\n!dog - sends a dog image\n!lizard - sends a lizard image\n!duck - sends a duck image.\n!rpanda - sends a red panda image\n!kangaroo - sends a kangaroo image\n!koala - sends a koala image\n!raccoon - sends a raccoon image```", inline: true },
+          { name: "Fact commands:", value: "```!catfact - sends a cat fact\n!dogfact - sends a dog fact\n!birdfact - sends a bird fact\n!pfact - sends a panda fact\n!kfact - sends a koala fact\n!rfact - sends a raccoon fact```", inline: true }
       )
       .setTimestamp()
       .setFooter({ text: "Version " + config.version });
