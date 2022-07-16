10e471b4f718a92f2294df99d5df8f70d0c3d74c
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-16 16:15:45 +0100

    Version 2.0.0-b1-16.07 rewrite

--- commands/raccoon.js
+++ commands/raccoon.js
@@ -1,30 +1,58 @@
-const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "raccoon",
-  description: "Command providing a random raccoon image",
-  execute(message, args, config) {
-    if (message.channel.id !== config.animal_images_channel) {
-      message
-        .reply(`This command can only be used in <#${config.animal_images_channel}>`)
-        .then((msg) => {
-          setTimeout(() => {
-            msg.delete();
-            message.delete();
-          }, 5000);
-        });
+const data = new SlashCommandBuilder()
+  .setName("raccoon")
+  .setDescription("Sends a random raccoon picture!");
+async function execute(client, interaction, subinteraction, config) {
+  async function get_raccoon_interaction() {
+    const row = new MessageActionRow();
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("newraccoon")
+        .setLabel("I want another one!")
+        .setStyle(1)
+        .setEmoji("🦝")
+    );
+    row.addComponents(
+      new MessageButton()
+        .setCustomId("delete")
+        .setLabel("Delete")
+        .setStyle(4)
+        .setEmoji("🗑️")
+    );
+    var response = await config.request("GET", "https://some-random-api.ml/img/raccoon");
+    var response = await response.json();
+    var raccoon_url = response.link;
+    var raccoonEmbed = new MessageEmbed()
+      .setColor(config.color)
+      .setTitle("Here's a raccoon!")
+      .setImage(raccoon_url)
+      .setTimestamp()
+      .setFooter({ text: "Powered by some-random-api.ml" });
+    return { embeds: [raccoonEmbed], components: [row] };
+  }
+  if (interaction.isButton()) {
+    if (interaction.user.id !== subinteraction.user.id) {
       return;
     }
-    config.request("GET", "https://some-random-api.ml/img/raccoon").then((response) => {
-      response.json().then((data) => {
-        const raccoonEmbed = new MessageEmbed()
-          .setColor(config.color)
-          .setTitle("Here's a raccoon!")
-          .setImage(data.link)
-          .setTimestamp()
-          .setFooter({ text: "Powered by some-random-api.ml" });
-        message.reply({ embeds: [raccoonEmbed] });
-      });
-    });
-  },
+    if (interaction.customId === "delete") {
+      return await interaction.message.delete();
+    } else if (interaction.customId === "newraccoon") {
+      await interaction.message.edit(await get_raccoon_interaction());
+      return await interaction.deferUpdate();
+    }
+  }
+    if (interaction.channel.id !== config.animal_images_channel) {
+        
+        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
+        
+      return;
+    }
+  await interaction.reply(await get_raccoon_interaction());
+}
+
+module.exports = {
+  data,
+  execute,
 };
