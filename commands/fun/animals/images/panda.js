1402c85e98e508ce12583da32823180f20a8aec1
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-17 12:29:47 +0100

    2.1.1

--- commands/fun/animals/images/panda.js
+++ commands/fun/animals/images/panda.js
@@ -1,16 +1,20 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+  SlashCommandBuilder,
+  SlashCommandBooleanOption,
+} = require("@discordjs/builders");
 const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
 const data = new SlashCommandBuilder()
-  .setName("redpanda")
-  .setDescription("Sends a random red panda picture!");
+  .setName("panda")
+  .setDescription("Sends a random panda picture!")
+  .addBooleanOption(option => option.setName('redpanda').setDescription('Sends a red panda picture instead of a regular panda'))
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_redpanda_interaction() {
     const row = new MessageActionRow();
     row.addComponents(
       new MessageButton()
-        .setCustomId("newredpanda")
+        .setCustomId("newpanda")
         .setLabel("I want another one!")
         .setStyle(1)
         .setEmoji("🐼")
@@ -22,12 +26,32 @@ async function execute(client, interaction, subinteraction, config) {
         .setStyle(4)
         .setEmoji("🗑️")
     );
-    var response = await config.request("GET", "https://some-random-api.ml/img/red_panda");
+    if (typeof interaction.options != "undefined") {
+      if (interaction.options.getBoolean("redpanda")) {
+        var url = "https://some-random-api.ml/img/red_panda";
+        var title = "Here's a red panda!";
+      } else {
+        var url = "https://some-random-api.ml/img/panda";
+        var title = "Here's a panda!";
+      }
+    } else {
+      if (interaction.message.embeds[0].title.includes("red")) {
+        var url = "https://some-random-api.ml/img/red_panda";
+        var title = "Here's a red panda!";
+      } else {
+        var url = "https://some-random-api.ml/img/panda";
+        var title = "Here's a panda!";
+      }
+    }
+    var response = await config.request(
+      "GET",
+      url
+    );
     var response = await response.json();
     var redpanda_url = response.link;
     var redpandaEmbed = new MessageEmbed()
       .setColor(config.color)
-      .setTitle("Here's a red panda!")
+      .setTitle(title)
       .setImage(redpanda_url)
       .setTimestamp()
       .setFooter({ text: "Powered by some-random-api.ml" });
@@ -39,17 +63,19 @@ async function execute(client, interaction, subinteraction, config) {
     }
     if (interaction.customId === "delete") {
       return await interaction.message.delete();
-    } else if (interaction.customId === "newredpanda") {
+    } else if (interaction.customId === "newpanda") {
       await interaction.message.edit(await get_redpanda_interaction());
       return await interaction.deferUpdate();
     }
   }
-    if (interaction.channel.id !== config.animal_images_channel) {
-        
-        await interaction.reply({ content: `This command can only be used in <#${config.animal_images_channel}>`, ephemeral: true });
-        
-      return;
-    }
+  if (interaction.channel.id !== config.animal_images_channel) {
+    await interaction.reply({
+      content: `This command can only be used in <#${config.animal_images_channel}>`,
+      ephemeral: true,
+    });
+
+    return;
+  }
   await interaction.reply(await get_redpanda_interaction());
 }
 
