8fe2d21c47bde201aae5dce2ee35048e168c86dc
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-21 11:27:00 +0100

    Discord.js 14 update

--- commands/fun/animals/facts/birdfact.js
+++ commands/fun/animals/facts/birdfact.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
   .setName("birdfact")
@@ -7,16 +7,16 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_bfact_interaction() {
-    const row = new MessageActionRow();
+    const row = new ActionRowBuilder();
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("newbfact")
         .setLabel("I want another one!")
         .setStyle(1)
         .setEmoji("🐦")
     );
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("delete")
         .setLabel("Delete")
         .setStyle(4)
@@ -25,7 +25,7 @@ async function execute(client, interaction, subinteraction, config) {
     var response = await config.request("GET", "https://some-random-api.ml/facts/bird");
     var response = await response.json();
     var bfact_url = response.fact;
-    var bfactEmbed = new MessageEmbed()
+    var bfactEmbed = new EmbedBuilder()
       .setColor(config.color)
       .setTitle("Here's a random bird fact!")
       .setDescription(bfact_url)
