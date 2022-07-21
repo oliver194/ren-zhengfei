7feeaecd8410b553c725e0e828f132e7296e7e47
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-21 11:28:31 +0100

    Discord.js 14 update

--- commands/fun/animals/facts/pandafact.js
+++ commands/fun/animals/facts/pandafact.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
   .setName("pandafact")
@@ -7,16 +7,16 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_pfact_interaction() {
-    const row = new MessageActionRow();
+    const row = new ActionRowBuilder();
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("newpfact")
         .setLabel("I want another one!")
         .setStyle(1)
         .setEmoji("🐼")
     );
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("delete")
         .setLabel("Delete")
         .setStyle(4)
@@ -25,7 +25,7 @@ async function execute(client, interaction, subinteraction, config) {
     var response = await config.request("GET", "https://some-random-api.ml/facts/panda");
     var response = await response.json();
     var pfact_url = response.fact;
-    var pfactEmbed = new MessageEmbed()
+    var pfactEmbed = new EmbedBuilder()
       .setColor(config.color)
       .setTitle("Here's a random panda fact!")
       .setDescription(pfact_url)
