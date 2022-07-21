0f16c9096b50b1a9370d128fc1a828a7b014bfad
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-21 11:26:27 +0100

    Discord.js 14 update

--- commands/neko.js
+++ commands/neko.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
   .setName("neko")
@@ -7,16 +7,16 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_neko_interaction() {
-    const row = new MessageActionRow();
+    const row = new ActionRowBuilder();
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("newneko")
         .setLabel("I want another one!")
         .setStyle(1)
         //.setEmoji("")
     );
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("delete")
         .setLabel("Delete")
         .setStyle(4)
@@ -25,7 +25,7 @@ async function execute(client, interaction, subinteraction, config) {
     var response = await config.request("GET", "https://nekos.life/api/v2/img/neko");
     var response = await response.json();
     var neko_url = response.url;
-    var nekoEmbed = new MessageEmbed()
+    var nekoEmbed = new EmbedBuilder()
       .setColor(config.color)
       .setTitle("Here's a neko!")
       .setImage(neko_url)
