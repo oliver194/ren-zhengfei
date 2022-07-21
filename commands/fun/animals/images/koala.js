97b46d3c603b926003a7b28a11ac663b132f0e7c
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-21 11:31:06 +0100

    Discord.js 14 update

--- commands/fun/animals/images/koala.js
+++ commands/fun/animals/images/koala.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
   .setName("koala")
@@ -7,16 +7,16 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_koala_interaction() {
-    const row = new MessageActionRow();
+    const row = new ActionRowBuilder();
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("newkoala")
         .setLabel("I want another one!")
         .setStyle(1)
         .setEmoji("🐨")
     );
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("delete")
         .setLabel("Delete")
         .setStyle(4)
@@ -25,7 +25,7 @@ async function execute(client, interaction, subinteraction, config) {
     var response = await config.request("GET", "https://some-random-api.ml/img/koala");
     var response = await response.json();
     var koala_url = response.link;
-    var koalaEmbed = new MessageEmbed()
+    var koalaEmbed = new EmbedBuilder()
       .setColor(config.color)
       .setTitle("Here's a koala!")
       .setImage(koala_url)
