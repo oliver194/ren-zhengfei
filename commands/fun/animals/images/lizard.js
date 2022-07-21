9567ffd81ef1dcdc570123764276726003d8635e
oliver194 <100048880+oliver194@users.noreply.github.com>
2022-07-21 11:31:30 +0100

    Discord.js 14 update

--- commands/fun/animals/images/lizard.js
+++ commands/fun/animals/images/lizard.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
   .setName("lizard")
@@ -7,16 +7,16 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction, subinteraction, config) {
   async function get_lizard_interaction() {
-    const row = new MessageActionRow();
+    const row = new ActionRowBuilder();
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("newlizard")
         .setLabel("I want another one!")
         .setStyle(1)
         .setEmoji("🐨")
     );
     row.addComponents(
-      new MessageButton()
+      new ButtonBuilder()
         .setCustomId("delete")
         .setLabel("Delete")
         .setStyle(4)
@@ -25,7 +25,7 @@ async function execute(client, interaction, subinteraction, config) {
     var response = await config.request("GET", "https://nekos.life/api/v2/img/lizard");
     var response = await response.json();
     var lizard_url = response.url;
-    var lizardEmbed = new MessageEmbed()
+    var lizardEmbed = new EmbedBuilder()
       .setColor(config.color)
       .setTitle("Here's a lizard!")
       .setImage(lizard_url)
